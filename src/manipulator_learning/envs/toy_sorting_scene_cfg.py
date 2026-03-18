1a0d1dca0691ae42b0fac591bd7b1bce718b478a
Ashok <ashok.kannan93@gmail.com>
2026-03-18 18:15:51 +0100

    test: ground+light+robot only, no lightwheel assets

--- src/manipulator_learning/envs/toy_sorting_scene_cfg.py
+++ src/manipulator_learning/envs/toy_sorting_scene_cfg.py
@@ -60,11 +60,11 @@ class ToySortingSceneCfg(InteractiveSceneCfg):
         prim_path="/World/light",
         spawn=sim_utils.DomeLightCfg(intensity=2000.0, color=(0.9, 0.9, 1.0)),
     )
-    table = AssetBaseCfg(
-        prim_path="{ENV_REGEX_NS}/Table",
-        spawn=sim_utils.UsdFileCfg(usd_path=_TABLE_USD),
-        init_state=AssetBaseCfg.InitialStateCfg(pos=(0.0, 0.0, 0.0)),
-    )
+    # table = AssetBaseCfg(
+    #     prim_path="{ENV_REGEX_NS}/Table",
+    #     spawn=sim_utils.UsdFileCfg(usd_path=_TABLE_USD),
+    #     init_state=AssetBaseCfg.InitialStateCfg(pos=(0.0, 0.0, 0.0)),
+    # )
 
     # Robot
     robot: ArticulationCfg = make_so_arm101_cfg(_ROBOT_URDF).replace(
